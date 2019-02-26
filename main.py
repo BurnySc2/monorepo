97afb62937d29aa1e0640398cee5d196b84e0b6a
BurnySc2 <gamingburny@gmail.com>
2019-02-26 14:41:02 +0100

    Add contracts and hypothesis

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+# Testing
+from dpcontracts import require, ensure
+from hypothesis import given
+from hypothesis.strategies import text, integers, floats
+
+# Coroutines and multiprocessing
 import asyncio
 import aiohttp
-import multiprocessing
+from multiprocessing import Process, Lock, Pool
 
 
 async def main():
@@ -9,6 +15,16 @@ async def main():
     await do_math(6)
 
 
+@require(
+    "First argument has to be a session object",
+    lambda args: isinstance(args.session, aiohttp.ClientSession),
+)
+@require("Second argument has to be a url", lambda args: isinstance(args.url, str))
+@require("Second argument can not be empty", lambda args: args.url != "")
+@ensure(
+    "Return value has to be a response",
+    lambda args, result: isinstance(result, aiohttp.ClientResponse),
+)
 async def download_site(session, url):
     async with session.get(url) as response:
         return response
@@ -22,9 +38,17 @@ async def download_all_sites(sites):
         #     tasks.append(download_site(session, url))
         responses = await asyncio.gather(*tasks, return_exceptions=True)
         response_lengths = [response.content_length for response in responses]
-        print(f"Response lengths: {response_lengths}")
+        # print(f"Response lengths: {response_lengths}")
+    return responses
 
 
+@require(
+    "Argument has to be a number", lambda args: isinstance(args.number, (int, float))
+)
+@ensure(
+    "Return value needs to be a number",
+    lambda args, result: isinstance(result, (int, float)),
+)
 async def do_math(number):
     return number + 3
 
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,16 +6,41 @@ sys.path.append("..")
 from main import do_math, download_site, download_all_sites
 import pytest
 import aiohttp
+from hypothesis import given
+import hypothesis.strategies as st
+
+website_content_length = 19210
 
 
 @pytest.mark.asyncio
 async def test_download_site():
     async with aiohttp.ClientSession() as session:
         res = await download_site(session, "http://www.jython.org")
-    assert 19210 == res.content_length
+    assert website_content_length == res.content_length
+
+
+@pytest.mark.asyncio
+async def test_download_all_sites():
+    urls = ["http://www.jython.org"] * 2
+    results = await download_all_sites(urls)
+    assert website_content_length * 2 == sum(
+        result.content_length for result in results
+    )
 
 
 @pytest.mark.asyncio
 async def test_do_math():
     res = await do_math(7)
     assert 10 == res
+
+
+@pytest.mark.asyncio
+@given(st.integers())
+async def test_do_math_integers(value):
+    assert 3 + value == await do_math(value)
+
+
+@pytest.mark.asyncio
+@given(st.floats(allow_infinity=False, allow_nan=False))
+async def test_do_math_floats(value):
+    assert 3 + value == await do_math(value)
