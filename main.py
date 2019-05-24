9198d8bcc235839568c7da3507dd8944d01c458e
BurnySc2 <gamingburny@gmail.com>
2019-05-24 17:35:58 +0200

    Add upload output example (#1)
    * Upload generated file
    
    * Add main.py run command
    
    * Add a tag to this commit
    
    * Disable tests for 3.6 and 3.8 to test 3.7 faster
    
    * Remove dpcontracts
    
    * Change command to use pipenv
    
    * Change commands again
    
    * Add ls command
    
    * Change path
    
    * Add quotation marks
    
    * Add another command using python module
    
    * Try to use cd command
    
    * Use workdir parameter https://docs.docker.com/engine/reference/run/#workdir
    
    * Use quotation marks
    
    * Replace equal sign with space
    
    * Move work directory before container name
    
    * Remove quotation marks
    
    * Add sh instead of bash command
    
    * Just try to upload main.py instead
    
    * Allow all branches
    
    * Change api key
    
    * Add quotation marks
    
    * Add GITHUB_OAUTH_TOKEN to travis build
    
    * Disable 3.7 tests and focus on getting upload to work
    
    * Add build script before deploy
    
    * Move comment
    
    * Turn multiline comment into single line comments
    
    * Add container creation
    
    * Remove -i
    
    * Add pipenv again
    
    * Add more install requirements
    
    * Add python -m before pipenv command
    
    * Add -it command
    
    * Specify python version
    
    * Generate requirements.txt from pipfile
    
    * Add quotation marks
    
    * Add python version parameter
    
    * Use pipenv -r lock to write requirements.txt
    
    * Remove test imports from main.py
    
    * Push a commit with tag
    
    * Push a commit with tag
    
    * Push a commit with tag
    
    * Push a commit with tag
    
    * Test if this is echod in deploy script
    
    * Test if statement in yml, travis should not run
    
    * DEPLY travis should run this time
    
    * DEPLY travis should run this time
    
    * DEPLOY travis should run this time
    
    * Remove if statement
    
    * Add if statement
    
    * Put condition in deploy part
    
    * DEPLOY remove echo
    
    * Does this deploy now
    
    * DEPLY this
    
    * DEPLOY this
    
    * Does it deploy without condition
    
    * DEPLOY please
    
    * DEPLOY do i have to use quotation?
    
    * DEPLOY does this work
    
    * DEPLOY use stages instead
    
    * DEPLOY comment out stages
    
    * Remove if
    
    * Add if statement
    
    * Remove tags
    
    * Does it only run 3.8 test now
    
    * DEPLOY does this work
    
    * DEPLOY add idna_ssl
    
    * DEPLOY clean up
    
    * DEPLOY clean up2
    
    * DEPLOY remove tags
    
    * DEPLOY enable all tests again

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Testing
-from dpcontracts import require, ensure
-from hypothesis import given
-from hypothesis.strategies import text, integers, floats
-
 # Coroutines and multiprocessing
 import asyncio
 import aiohttp
@@ -12,7 +7,7 @@ from multiprocessing import Process, Lock, Pool
 from typing import List, Iterable, Union
 
 # Other
-import time
+import time, os, sys
 
 
 async def main():
@@ -29,11 +24,10 @@ async def main():
     end_time = time.perf_counter()
     print(f"Time for multiprocessing taken: {end_time - start_time}")
 
+    print(f"Creating hello world file...")
+    create_file()
 
-@require("First argument has to be a session object", lambda args: isinstance(args.session, aiohttp.ClientSession))
-@require("Second argument has to be a url", lambda args: isinstance(args.url, str))
-@require("Second argument can not be empty", lambda args: args.url != "")
-@ensure("Return value has to be a response", lambda args, result: isinstance(result, aiohttp.ClientResponse))
+
 async def download_site(session: aiohttp.ClientSession, url: str) -> aiohttp.ClientResponse:
     async with session.get(url) as response:
         return response
@@ -60,20 +54,14 @@ async def download_all_sites(sites: Iterable[str]) -> List[aiohttp.ClientRespons
     return responses
 
 
-@require("Argument has to be a number", lambda args: isinstance(args.number, (int, float)))
-@ensure("Return value needs to be a number", lambda args, result: isinstance(result, (int, float)))
 async def do_math(number: Union[int, float]) -> Union[int, float]:
     return number + 3
 
 
-@require("Argument has to be integer", lambda args: isinstance(args.number, int))
-@ensure("Returnvalue has to be integer", lambda args, result: isinstance(result, int))
-@ensure("Returnvalue has to be zero or larger than zero", lambda args, result: result >= 0)
 def cpu_bound_summing(number: int) -> int:
     return sum(i * i for i in range(number))
 
 
-@require("Argument has to be iterable", lambda args: iter(args.numbers))
 def find_sums(numbers: Iterable[int]) -> List[int]:
     with Pool() as pool:
         result = pool.map(cpu_bound_summing, numbers)
@@ -83,6 +71,14 @@ def find_sums(numbers: Iterable[int]) -> List[int]:
 def do_multiprocessing():
     numbers: List[int] = [5_000_000 + x for x in range(20)]
     sums: List[int] = find_sums(numbers)
+
+
+def create_file():
+    path = os.path.dirname(__file__)
+    example_file_path = os.path.join(path, "hello_world.txt")
+    with open(example_file_path, "w") as f:
+        f.write("Hello world!\n")
+    print(f"Hello world file created in path {example_file_path}")
 
 
 if __name__ == "__main__":
