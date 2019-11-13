2ed7c31d3fdd378113164a568ac8c2a20da74c34
BuRny <gamingburny@gmail.com>
2019-11-13 16:51:42 +0100

    Add stack functions

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -15,21 +15,41 @@ class Stack:
         If the stack was previously empty, self.top will point towards the first item.
         If the stack was not empty, self.top will point towards the newly pushed item and that item will point towards the previously first item.
         """
+        if not self.top:
+            self.top = Item(value)
+        else:
+            new_item = Item(value)
+            new_item.next = self.top
+            self.top = new_item
 
     def pop(self) -> int:
         """
         Removes the item (value) on top and returns it.
         """
+        if self.top:
+            return_item: Item = self.top
+            self.top = return_item.next
+            return return_item.value
+        raise IndexError("pop from empty stack")
 
     def is_empty(self) -> bool:
         """
         Checks if the stack is empty, returns True or False.
         """
+        return not self.top
 
     def size(self) -> int:
         """
         Checks the size of the stack, how many items are there. Returns an integer.
         """
+        if not self.top:
+            return 0
+        item: Item = self.top
+        count = 1
+        while item.next:
+            item = item.next
+            count += 1
+        return count
 
 
 if __name__ == "__main__":
@@ -41,7 +61,6 @@ if __name__ == "__main__":
     assert (
         top == 5
     ), f"Push function does not work as intended, value on top of the stack should be 5 but is actually ({top}) (top value exists: {bool(s.top)})"
-    assert s.top and s.top.value == 5
 
     s.push(8)
     first_element = s.top and s.top.value
@@ -68,7 +87,7 @@ if __name__ == "__main__":
 
     s.pop()
     is_empty = s.is_empty()
-    assert is_empty is False, f"is_empty() function should return True but gave back ({is_empty})"
+    assert is_empty is True, f"is_empty() function should return True but gave back ({is_empty})"
 
     size = s.size()
     assert size == 0, f"size() function should return 0 but gave back ({size})"
