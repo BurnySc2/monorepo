2bbd66a66f91c888089e80a77bddc0cc2f9b493e
BurnySc2 <gamingburny@gmail.com>
2021-11-13 22:05:07 +0100

    Add prettier to svelte

--- svelte_frontend/src/pages/About.svelte
+++ svelte_frontend/src/pages/About.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-  export let defaultText: string = "this text";
-  let someText = defaultText;
+    export let defaultText: string = "this text"
+    let someText = defaultText
 </script>
 
-<h1 class="text-center my-2">
-  This is my about page! Here I display features of svelte that I have
-  implemented
-</h1>
+<h1 class="text-center my-2">This is my about page! Here I display features of svelte that I have implemented</h1>
 <button
-  class="p1 rounded"
-  on:click={() => {
-    someText = someText === defaultText ? "is changing" : defaultText;
-  }}
+    class="p1 rounded"
+    on:click={() => {
+        someText = someText === defaultText ? "is changing" : defaultText
+    }}
 >
-  Click me: {someText}
+    Click me: {someText}
 </button>
