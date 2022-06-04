9211935485b6d862e67f6744c54872dce5c85857
burny <gamingburny@gmail.com>
2022-06-04 19:28:00 +0200

    Add prod env variable

--- fastapi_server/README.md
+++ fastapi_server/README.md
@@ -12,12 +12,18 @@ Now you can go to `http://0.0.0.0:8000` or `http://0.0.0.0:8000/docs` to check o
 
 On the server, either build the docker image via
 
-`docker build -t burnysc2/fastapi_server:latest .`
+```
+docker build -t burnysc2/fastapi_server:latest .
+```
 
 or pull the latest image
 
-`docker pull burnysc2/fastapi_server:latest`
+```
+docker pull burnysc2/fastapi_server:latest
+```
 
 now run it with a `data` subfolder mounted which will be persistent
 
-`docker run -it -d --rm --name fastapitest -p 8000:8000 --mount type=bind,source="$(pwd)/data",destination=/root/fastapi_server/data burnysc2/fastapi_server:latest`
+```
+docker run --rm --name fastapitest --publish 8000:8000 --env STAGE=PROD --mount type=bind,source="$(pwd)/data",destination=/root/fastapi_server/data burnysc2/fastapi_server:latest
+```
--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -1,7 +1,7 @@
-from pathlib import Path
+import os
+from typing import Literal
 
 import uvicorn
-from dotenv import dotenv_values
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
@@ -11,24 +11,23 @@ from fastapi_server.routes.hello_world import hello_world_router
 from fastapi_server.routes.replay_parser import replay_parser_router
 from fastapi_server.routes.todolist import todo_list_router
 
-env_path = Path(__file__).parent / '.env'
-if not env_path.is_file():
-    logger.info(
-        """Local .env file in "fastapi_server" directory does not exist. For local development please fill it with contents:
-STAGE=dev"""
-    )
-
-config: dict = dotenv_values(env_path)
+STAGE: Literal['DEV', 'PROD'] = os.getenv('STAGE', 'DEV')
 
 app = FastAPI()
 app.include_router(hello_world_router)
 app.include_router(replay_parser_router)
+app.include_router(todo_list_router)
 
-origins = ['https://burnysc2.github.io', 'https://replaycomparer.netlify.app']
-if config.get('STAGE', '') != 'prod':
-    logger.info("Starting in 'STAGE == dev' mode")
+origins = [
+    'https://burnysc2.github.io',
+    'https://replaycomparer.netlify.app',
+    'https://burnysc2-monorepo.netlify.app',
+    'https://burnysc2-monorepo-dev.netlify.app',
+]
+
+logger.info(f"Starting in 'STAGE == {STAGE}' mode")
+if STAGE != 'prod':
     origins += [f'http://localhost:{i}' for i in range(1, 2**16)]
-    app.include_router(todo_list_router)
     app.include_router(chat_router)
 
 app.add_middleware(
