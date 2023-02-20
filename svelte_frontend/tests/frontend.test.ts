d2601c6227e2f06ec3078519b408f92344e7eaa3
burny <gamingburny@gmail.com>
2023-02-20 03:39:41 +0100

    Add waitForTimeout in frontend tests

--- svelte_frontend/tests/frontend.test.ts
+++ svelte_frontend/tests/frontend.test.ts
@@ -64,6 +64,7 @@ test("todo page works", async ({ page }) => {
     await page.fill("#newTodoInput", "test todo1")
     expect(await page.inputValue("#newTodoInput")).toBe("test todo1")
     await page.click("#submit1")
+    await page.waitForTimeout(100)
     expect(await page.inputValue("#newTodoInput")).toBe("")
     expect(await page.innerText("body")).toContain("test todo1")
 
@@ -73,6 +74,7 @@ test("todo page works", async ({ page }) => {
     await page.fill("#newTodoInput", "test todo2")
     expect(await page.inputValue("#newTodoInput")).toBe("test todo2")
     await page.click("#submit2")
+    await page.waitForTimeout(100)
     expect(await page.inputValue("#newTodoInput")).toBe("")
     expect(await page.innerText("body")).toContain("test todo2")
 
@@ -82,6 +84,7 @@ test("todo page works", async ({ page }) => {
     await page.fill("#newTodoInput", "test todo3")
     expect(await page.inputValue("#newTodoInput")).toBe("test todo3")
     await page.click("#submit3")
+    await page.waitForTimeout(100)
     expect(await page.inputValue("#newTodoInput")).toBe("")
     expect(await page.innerText("body")).toContain("test todo3")
 })
@@ -118,19 +121,30 @@ for (const { increase, decrease } of browserstorageTries) {
         await page.goto("/browserstorage")
 
         expect(await page.innerText("#localStorageValue")).toBe("0")
-        await page.click("#increaseLocalStorage", { clickCount: increase })
-        expect(await page.innerText("#localStorageValue")).toBe(increase.toString())
-        await page.click("#decreaseLocalStorage", { clickCount: decrease })
-        expect(await page.innerText("#localStorageValue")).toBe((increase - decrease).toString())
-        await page.click("#resetLocalStorage")
-        expect(await page.innerText("#localStorageValue")).toBe("0")
 
+        await page.click("#increaseLocalStorage", { clickCount: increase })
+        await page.waitForTimeout(100)
+        expect(await page.innerText("#localStorageValue")).toBe(increase.toString())
+
+        await page.click("#decreaseLocalStorage", { clickCount: decrease })
+        await page.waitForTimeout(100)
+        expect(await page.innerText("#localStorageValue")).toBe((increase - decrease).toString())
+
+        await page.click("#resetLocalStorage")
+        await page.waitForTimeout(100)
+        expect(await page.innerText("#localStorageValue")).toBe("0")
         expect(await page.innerText("#sessionStorageValue")).toBe("0")
+
         await page.click("#increaseSessionStorage", { clickCount: increase })
+        await page.waitForTimeout(100)
         expect(await page.innerText("#sessionStorageValue")).toBe(increase.toString())
+
         await page.click("#decreaseSessionStorage", { clickCount: decrease })
+        await page.waitForTimeout(100)
         expect(await page.innerText("#sessionStorageValue")).toBe((increase - decrease).toString())
+
         await page.click("#resetSessionStorage")
+        await page.waitForTimeout(100)
         expect(await page.innerText("#sessionStorageValue")).toBe("0")
     })
 }
