79172ba26fd184c4a93c6dd858a46aae8dcf33ec
burny <gamingburny@gmail.com>
2023-06-27 22:27:03 +0200

    Reduce factor in an attempt to fix tests

--- python_examples/main.py
+++ python_examples/main.py
@@ -60,7 +60,7 @@ async def main():
 
     regex_match_test()
 
-    sites: list[str] = ["http://www.jython.org", "https://www.python.org/"] * 50
+    sites: list[str] = ["http://www.jython.org", "https://www.python.org/"] * 20
     start_time = time.perf_counter()
     await download_all_sites(sites)
 
