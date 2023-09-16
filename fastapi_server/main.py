e99c345b324281ad7db0628512f6bc40bc88ca86
burny <gamingburny@gmail.com>
2023-09-16 02:27:09 +0200

    Set allow_credentials to true

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -46,7 +46,7 @@ if STAGE != "PROD":
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    # allow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
