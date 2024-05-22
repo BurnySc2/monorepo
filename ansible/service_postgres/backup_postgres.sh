04b5f293d89d1063a18ab7795de27444160be5ee
burny <gamingburny@gmail.com>
2024-05-22 17:42:39 +0200

    Pipe docker output to 7z to add compression and encryption to backup

--- ansible/service_postgres/backup_postgres.sh
+++ ansible/service_postgres/backup_postgres.sh
@@ -1,8 +1,19 @@
 # Exit the script if any command fails
 set -e
 
+PASSWORD="$1"
+
 BACKUP_FOLDER="{{ secrets.POSTGRES.BACKUP_MOUNT_PATH }}"
-BACKUP_PATH="${BACKUP_FOLDER}/$(date +'%Y-%m-%d_%H:%M:%S').tar"
+
+if [ "$(date +%d)" -eq 01 ]; then
+    # Monthly backup on the first day of the month
+    BACKUP_PATH="${BACKUP_FOLDER}/POSTGRES_MONTHLY_$(date +'%Y-%m-%d_%H:%M:%S').zip"
+elif [ "$(date +%u)" -eq 7 ]; then
+    # Create weekly backup on Sundays
+    BACKUP_PATH="${BACKUP_FOLDER}/POSTGRES_WEEKLY_$(date +'%Y-%m-%d_%H:%M:%S').zip"
+else
+    BACKUP_PATH="${BACKUP_FOLDER}/POSTGRES_DAILY_$(date +'%Y-%m-%d_%H:%M:%S').zip"
+fi
 
 # Create folder if it doesn't exist, and make syncthing owner
 mkdir -p $BACKUP_FOLDER
@@ -11,23 +22,44 @@ chown syncthing:syncthing $BACKUP_FOLDER
 chmod 700 $BACKUP_FOLDER
 
 # https://medium.com/@burakkocakeu/get-pg-dump-from-a-docker-container-and-pg-restore-into-another-in-5-steps-74ca5bf0589c
+# Backup
 CONTAINER_NAME="postgres_postgres"
-docker exec $CONTAINER_NAME pg_dump -U postgres -F t postgres > $BACKUP_PATH
+backupargs=(
+    exec $CONTAINER_NAME 
+    pg_dump 
+    # User
+    -U postgres
+    # Format
+    -F t
+    # ?
+    postgres
+)
+# Compress and encrypt
+zipargs=(
+    a
+    # Set archive type
+    -t7z
+    # Require password before seeing files: hide file structure
+    -mhe=on
+    # Max compression level
+    -mx9
+    # Set password
+    "-p$PASSWORD"
+    # Input from stdin to be able to pipe
+    -si
+    $BACKUP_PATH
+)
+# Compress .tar file with 7z
+docker "${backupargs[@]}" | 7z "${zipargs[@]}"
+
 # Make owner syncthing so that it can sync properly
 chown syncthing:syncthing $BACKUP_PATH
 # Only owner can read/write, group can read
 chmod 640 $BACKUP_PATH
 
-# Remove backups with file size < 1kb (= failed backups)
-cd $BACKUP_FOLDER
-find . -maxdepth 1 -type f -name '*.tar' -size -1024c -delete
-
 # Keep up to 3 backups newest, remove other
-MAX_BACKUPS_TO_KEEP=3
-FILES_TO_REMOVE=$(ls -t *.tar | tail -n +$(expr $MAX_BACKUPS_TO_KEEP + 1))
-if [ -n "$FILES_TO_REMOVE" ]; then
-    for file in $FILES_TO_REMOVE; do 
-        echo "Removing older .tar file: $file"
-        rm $file
-    done 
-fi
+MAX_BACKUPS=3
+cd $BACKUP_FOLDER
+ls $BACKUP_FOLDER | grep "_DAILY_" | head -n "-$MAX_BACKUPS" | xargs rm -f --
+ls $BACKUP_FOLDER | grep "_WEEKLY_" | head -n "-$MAX_BACKUPS" | xargs rm -f --
+ls $BACKUP_FOLDER | grep "_MONTHLY_" | head -n "-$MAX_BACKUPS" | xargs rm -f --
