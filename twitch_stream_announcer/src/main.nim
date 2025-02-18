b7692d9193f9a75157ede46664ec4293225729e5
burnysc2 <gamingburny@gmail.com>
2025-02-18 03:47:42 +0100

    Fix image of mariadb in bookstack service

--- twitch_stream_announcer/src/main.nim
+++ twitch_stream_announcer/src/main.nim
@@ -320,7 +320,7 @@ proc run_for_one_hour(db: DbConn) =
       break
     echo "Running once"
     run_once(db)
-    sleep 10 * 1000
+    sleep 60 * 1000
 
 proc main(db: DbConn) =
   if STAGE == "DEV":
