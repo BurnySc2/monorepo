7d744234707ce55d6caf61ebf2a7d234eb7674b3
burnysc2 <gamingburny@gmail.com>
2025-03-11 20:42:27 +0100

    Only create sinks and loopback if they don't exist yet

--- ansible/setup_pc/setup_arch/roles/03_packages/tasks/audio.sh
+++ ansible/setup_pc/setup_arch/roles/03_packages/tasks/audio.sh
@@ -1,8 +1,17 @@
+# https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/
+# pactl list short sinks
+# pactl list short sources
+
 # Create audio loopbacks for twitch recording
 # Create sink, description is the name it shows in pulseaudio
-pactl load-module module-null-sink sink_name=Twitch sink_properties=device.description=Twitch
-pactl load-module module-null-sink sink_name=TwitchNoVod sink_properties=device.description=TwitchNoVod
+if ! pactl list sources short | grep -q "Twitch_Sink.monitor"; then
+    pactl load-module module-null-sink sink_name=Twitch_Sink sink_properties=device.description=Twitch_Sink
+    # Create loopback from source=devicename, find devicename from: 'pactrl list sources'
+    pactl load-module module-loopback source=Twitch_Sink.monitor
+fi
 
-# Create loopback from source=devicename, find devicename from: 'pactrl list sources'
-pactl load-module module-loopback source=Twitch.monitor
-pactl load-module module-loopback source=TwitchNoVod.monitor
+if ! pactl list sources short | grep -q "TwitchNoVod_Sink.monitor"; then
+    pactl load-module module-null-sink sink_name=TwitchNoVod_Sink sink_properties=device.description=TwitchNoVod_Sink
+    # Create loopback from source=devicename, find devicename from: 'pactrl list sources'
+    pactl load-module module-loopback source=TwitchNoVod_Sink.monitor
+fi
