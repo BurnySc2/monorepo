c8f5e17bbf8a2fdb92a9ea52e3389c36167f63df
burnysc2 <gamingburny@gmail.com>
2025-05-11 02:52:55 +0200

    Update transcribe project

--- transcribe_website/transcriber_backend/src/transcribe_file.py
+++ transcribe_website/transcriber_backend/src/transcribe_file.py
@@ -3,13 +3,17 @@ import os
 from pathlib import Path
 from typing import Literal
 
-import whisperx  # pyre-fixme[21]
 from dotenv import load_dotenv
 from loguru import logger
+from whisperx.alignment import align, load_align_model
+from whisperx.asr import load_model
+from whisperx.audio import load_audio
 from whisperx.types import AlignedTranscriptionResult, SingleAlignedSegment, SingleWordSegment
 
+from hard_burn_subtitles import hard_burn_subtitles
 from prisma import Prisma
 from src.helper import recurse_path
+from srt_writer import get_srt_content
 
 load_dotenv()
 
@@ -22,13 +26,15 @@ type model_sizes = Literal[
     "small",
     "medium",
     "large",
+    # turbo model works only well for transcribing (all languages)
     "turbo",
+    # Only works for transcribing english
     "tiny.en",
     "base.en",
     "small.en",
     "medium.en",
     # https://huggingface.co/distil-whisper/distil-large-v2
-    # Only avaliable for english!
+    # Only works for transcribing english
     "distil-large-v2",
     "distil-large-v3",
     "distil-medium.en",
@@ -40,12 +46,13 @@ type language_codes = Literal["en", "fr", "de", "es", "it", "ja", "zh", "nl", "u
 def transcribe_file(
     file_path: Path,
     model_size: model_sizes,
+    task: Literal["transcribe", "translate"] = "transcribe",
     language: language_codes | None = None,
 ) -> AlignedTranscriptionResult:
     # Models: https://github.com/openai/whisper#available-models-and-languages
 
     device = "cpu"
-    batch_size = 4
+    batch_size = 8
     compute_type = "int8"
 
     # save model to local path (optional)
@@ -56,17 +63,17 @@ def transcribe_file(
     # https://github.com/m-bain/whisperX/issues/466
     # Maybe using model that only supports english?
 
-    model = whisperx.load_model(model_size, device, compute_type=compute_type, download_root=model_dir)
+    model = load_model(model_size, device, compute_type=compute_type, download_root=model_dir)
 
-    audio = whisperx.load_audio(file_path)
-    result = model.transcribe(audio, batch_size=batch_size, language=language)
+    audio = load_audio(file_path)
+    result = model.transcribe(audio, batch_size=batch_size, language=language, task=task)
 
     # delete model if low on GPU resources
     # import gc; gc.collect(); torch.cuda.empty_cache(); del model
 
     # Align whisper output
-    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
-    result2 = whisperx.align(
+    model_a, metadata = load_align_model(language_code=result["language"], device=device)
+    result2: AlignedTranscriptionResult = align(
         result["segments"],
         model_a,
         metadata,
@@ -147,14 +154,34 @@ def mass_transcribe(
 
 if __name__ == "__main__":
     # Can be run with:
-    # PYTHONPATH=$(pwd) poetry run python src/transcribe_file.py
+    # PYTHONPATH=$(pwd) uv run python src/transcribe_file.py
+
+    # This example mass transcribes files and loads them into database for later use
+
     input_folder_path = Path(os.getenv("MASS_TRANSCRIBE_INPUT_DIRECTORY"))
     # mass_transcribe(input_folder_path, model_size="tiny", language="de")
-    mass_transcribe(input_folder_path, model_size="medium", language="de")
+    # mass_transcribe(input_folder_path, model_size="medium", language="de")
 
-    # subtitle_path = audio_file_path.parent / f"{audio_file_path.stem}.srt"
+    # Example of writing german subtitles
+    # input_file_path = Path("my_file.mp4")
+    # subtitle_path = input_file_path.parent / f"{input_file_path.stem}_DE.srt"
+    # if not subtitle_path.is_file():
+    #     result = transcribe_file(input_file_path, model_size="turbo", language="de")
+    #     subtitle_data = get_srt_content(result)
+    #     subtitle_path.write_text(subtitle_data.getvalue().decode())
+    # output_file_path = input_file_path.parent / f"{input_file_path.stem}_DE.mp4"
+    # hard_burn_subtitles(input_file_path, subtitle_path, output_file_path)
 
-    # result = transcribe_file(audio_file_path, model_size="small", language="de")
-    # # save_result_to_db(audio_file_path, result)
-    # data = get_srt_content(result)
-    # subtitle_path.write_text(data.getvalue().decode())
+    # Example of writing english subtitles
+    input_file_path = Path("my_file.mp4")
+    subtitle_path = input_file_path.parent / f"{input_file_path.stem}_EN.srt"
+    if not subtitle_path.is_file():
+        # "language" specifies the input language,
+        # task "translate" translates to english (no other language possible as of yet)
+        logger.info("Started translating")
+        result = transcribe_file(input_file_path, task="translate", model_size="medium", language="de")
+        subtitle_data = get_srt_content(result)
+        subtitle_path.write_text(subtitle_data.getvalue().decode())
+        logger.info("Done translating")
+    output_file_path = input_file_path.parent / f"{input_file_path.stem}_EN.mp4"
+    hard_burn_subtitles(input_file_path, subtitle_path, output_file_path)
