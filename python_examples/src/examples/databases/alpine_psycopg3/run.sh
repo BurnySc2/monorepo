c439f7513b3f07937735d46ffe9966f1acc0c0a0
burnysc2 <gamingburny@gmail.com>
2025-08-05 15:22:55 +0200

    Add piccolo postgres example to test conversion types

--- python_examples/src/examples/databases/alpine_psycopg3/run.sh
+++
@@ -1,8 +0,0 @@
-docker build -t temp .
-
-# Compress image with slim
-slim build --target temp --tag temp_slim --http-probe=false --env-file "../../../../.env" --exec "python /root/main.py"
-# Ideally the following but it doesnt work, missing imports
-# slim build --target temp --tag temp_slim --http-probe=false --env STAGE=BUILD --exec "python /root/main.py"
-
-docker run --rm -it --env-file "../../../../.env" temp_slim
---
+++ python_examples/src/examples/databases/piccolo_postgres_example.py
@@ -0,0 +1,66 @@
+import asyncio
+import os
+from pathlib import Path
+
+from piccolo.columns import (
+    Integer,
+    Text,
+    JSON,
+    JSONB,
+    Bytea,
+    Boolean,
+    Timestamp,
+    Timestamptz,
+    DoublePrecision,
+    UUID,
+)
+from piccolo.engine.postgres import PostgresEngine
+from piccolo.table import Table, create_db_tables
+import arrow
+
+
+# Normally set in "piccolo_conf.py"
+db_path = Path(__file__).parent / "temp.db"
+DB = PostgresEngine(
+    config={
+        "dsn": os.getenv("POSTGRES_CONNECTION_STRING"),
+        "database": "temp_db",
+    }
+)
+# APP_REGISTRY = AppRegistry(apps=[])
+
+
+class TestTable(Table, db=DB):
+    # "id" field automatically gets added by piccolo
+    # id = Serial(required=True)
+    temp_bool = Boolean(default=False)
+    # Default int4
+    temp_int = Integer(default=42)
+    # Default float8
+    temp_float = DoublePrecision(default=3.14)
+    temp_text = Text(default="test")
+    temp_bytes = Bytea(default=b"test")
+    # Smaller than 'jsonb'
+    temp_json = JSON(default={"hello": "world"})
+    # Can be parsed compared to 'json'
+    temp_jsonb = JSONB(default={"hello": "world"})
+    temp_uuid = UUID()
+    temp_timestamp = Timestamp(default=arrow.utcnow().naive)
+    temp_timestamptz = Timestamptz(default=arrow.utcnow().datetime)
+
+
+async def run_database_with_piccolo():
+    await create_db_tables(TestTable, if_not_exists=True)
+
+    count = await TestTable().count()
+    if count < 1:
+        # Insert example
+        await TestTable().save()
+
+    # Retrieve example
+    temp_row = await TestTable.objects().first()
+    "debug entrypoint"
+
+
+if __name__ == "__main__":
+    asyncio.run(run_database_with_piccolo())
