fd09c10d0f3ab595f5e6ce9cbd8ca37e1a47dc02
burnysc2 <gamingburny@gmail.com>
2025-08-19 03:57:29 +0200

    Improve example hypothesis test

--- fastapi_server/test/test_hypothesis.py
+++ fastapi_server/test/test_hypothesis.py
@@ -2,58 +2,75 @@ from unittest import TestCase
 
 import hypothesis.strategies as st
 from hypothesis import given, settings
+from loguru import logger
 
 
 class TestHypothesis(TestCase):
+    """
+    Setup and teardown happen as described in this file.
+    You can verify this by running
+    uv run pytest -s test/test_hypothesis.py
+    """
+
     class_number = 0
     class_number_pytest = 0
-    method_number = 0
     method_number_pytest = 0
+    method_number = 0
     example_number = 0
 
     @classmethod
     def setUpClass(cls):
+        logger.info("1) Setting up class1")
         cls.class_number += 1
 
     @classmethod
     def tearDownClass(cls):
+        logger.info("Teardown class1")
         cls.class_number -= 1
 
     @classmethod
     def setup_class(cls):
-        cls.class_number_pytest += 2
+        logger.info("2) Setting up class2")
+        cls.class_number_pytest += 1
 
     @classmethod
     def teardown_class(cls):
-        cls.class_number_pytest -= 2
+        logger.info("Teardown class2")
+        cls.class_number_pytest -= 1
+
+    def setup_method(self, _method):
+        logger.info("3) Setting up method1")
+        self.method_number_pytest += 1
+
+    def teardown_method(self, _method):
+        logger.info("Teardown method1")
+        self.method_number_pytest -= 1
 
     @classmethod
     def setUp(cls):
-        cls.method_number += 3
+        logger.info("4) Setting up method2")
+        cls.method_number += 1
 
     @classmethod
     def tearDown(cls):
-        cls.method_number -= 3
-
-    def setup_method(self, _method):
-        self.method_number_pytest += 4
-
-    def teardown_method(self, _method):
-        self.method_number_pytest -= 4
+        logger.info("Teardown method2")
+        cls.method_number -= 1
 
     @classmethod
     def setup_example(cls):
-        cls.example_number += 5
+        logger.info("5) Setting up example")
+        cls.example_number += 1
 
     @classmethod
     def teardown_example(cls, _token=None):
-        cls.example_number -= 5
+        logger.info("Teardown example")
+        cls.example_number -= 1
 
-    @settings(max_examples=100)
+    @settings(max_examples=2)
     @given(_number=st.integers())
     def test_hypothesis(self, _number: int):
-        assert self.class_number == 1, "a"
-        assert self.class_number_pytest == 2, "b"
-        assert self.method_number == 3, "c"
-        assert self.method_number_pytest == 4, "d"
-        assert self.example_number == 5, "e"
+        assert self.class_number == 1, self.class_number
+        assert self.class_number_pytest == 1, self.class_number_pytest
+        assert self.method_number_pytest == 1, self.method_number_pytest
+        assert self.method_number == 1, self.method_number
+        assert self.example_number == 1, self.example_number
